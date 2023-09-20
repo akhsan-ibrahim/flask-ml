8ac04691b90eb24842041896b37f12a6bba25b98
Akhsan Ibrahim <akhsanibra08@gmail.com>
2023-09-20 09:50:36 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for, request
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def index():
+  return 'Hello Gender Classifier'
+
+if __name__ == '__main__':
+  app.run(debug=True)
